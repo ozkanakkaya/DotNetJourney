b192cf385372718953d55b54684064d04e267954
Ozkan Akkaya <33011814+ozkanakkaya@users.noreply.github.com>
2023-12-05 18:17:36 +0300

    Services.Catalog - Mapping işlemleri ve appsettings.json configurationları

--- Udemy.Microservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Udemy.Microservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,11 +1,27 @@
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Catalog", Version = "v1" });
+});
 
 var app = builder.Build();
 
